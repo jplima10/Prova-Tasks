ed8a682a06316964f81fdef830372a6881d81b06
João pedro Lima <joaopedrofontinelelima99@gmail.com>
2023-06-01 01:49:16 -0300

    Teste Prova

---
+++ src/config/firebaseconfig.js
@@ -0,0 +1,16 @@
+import firebase from "firebase"
+import "firebase/storage"
+
+var firebaseConfig = {
+    apiKey: "AIzaSyA0nESDyr6GbtC0rC7rGqnjyx6bFNe8jkE",
+    authDomain: "tasks-9520d.firebaseapp.com",
+    projectId: "tasks-9520d",
+    storageBucket: "tasks-9520d.appspot.com",
+    messagingSenderId: "665678694351",
+    appId: "1:665678694351:web:f6fb5461b2f614138274ba"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const database = firebase.firestore()
+  export default database
